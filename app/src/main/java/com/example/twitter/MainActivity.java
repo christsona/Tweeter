7a3ea0c3a2d0ca3506db6defdb96509db2cec3d8
Aaron <christsona@berea.edu>
2019-11-01 14:20:00 -0400

    Added gif walkthrough

--- app/src/main/java/com/example/twitter/MainActivity.java
+++ app/src/main/java/com/example/twitter/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.codepath.apps.restclienttemplate.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
